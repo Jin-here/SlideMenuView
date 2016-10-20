6c73026acb5dd24c85f485c8282d06da0688ed96
caojin <350194214@qq.com>
2016-10-20 13:35:09 +0800

    1.修复-已知bug; 2.增加-多种方案，适应不同场景，可通过关键字“方案”进行搜索;

--- app/src/main/java/com/vgaw/slidemenuview/SlideMenuView.java
+++ app/src/main/java/com/vgaw/slidemenuview/SlideMenuView.java
@@ -13,13 +13,13 @@ import android.widget.LinearLayout;
 /**
  * you only need to configure two attrs:
  * direction: the alignment, can either be left, right, top, bottom
- * isIn: a boolean value that tells whether the {@link SlideMenuView} will show to the user
+ * isShow: a boolean value that tells whether the {@link SlideMenuView} will show to the user
  */
 public class SlideMenuView extends LinearLayout{
     // 滑入滑出方向
     public static final int DIRECTION_LEFT = 0;
     public static final int DIRECTION_RIGHT = 1;
-    public static final int DIRECTION_UP = 2;
+    public static final int DIRECTION_TOP = 2;
     public static final int DIRECTION_BOTTOM = 3;
 
     private int mWidth;
@@ -30,14 +30,16 @@ public class SlideMenuView extends LinearLayout{
     private int bottomMargin;
 
     private boolean isIn = false;
+
     private int direction;
+    private boolean isShow;
 
     public SlideMenuView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SlideMenuView);
         direction = a.getInteger(R.styleable.SlideMenuView_direction, -1);
-        isIn = a.getBoolean(R.styleable.SlideMenuView_isIn, true);
+        isShow = a.getBoolean(R.styleable.SlideMenuView_isShow, true);
         a.recycle();
         if (direction == -1){
             throw new RuntimeException("the direction of the SlideMenuView should be set");
@@ -54,30 +56,91 @@ public class SlideMenuView extends LinearLayout{
         this.rightMargin = params.rightMargin;
         this.topMargin = params.topMargin;
         this.bottomMargin = params.bottomMargin;
-        slide();
+        if (!isShow){
+            slide();
+        }
     }
 
     public void slide(){
         // 始终为正数
-        float distance;
         switch (direction){
-            case DIRECTION_UP:
-                distance = topMargin + mHeight;
-                animate().yBy(isIn ? distance : -distance).setInterpolator(new OvershootInterpolator()).start();
+            case DIRECTION_TOP:
+                animate().y(isIn ? (topMargin + mHeight) : -(bottomMargin + mHeight)).setInterpolator(new OvershootInterpolator()).start();
                 break;
             case DIRECTION_BOTTOM:
-                distance = bottomMargin + mHeight;
-                animate().yBy(isIn ? -distance : distance).setInterpolator(new OvershootInterpolator()).start();
+                animate().y(isIn ? (DensityUtil.getScreenHeight(getContext()) - (mHeight + bottomMargin)) : (DensityUtil.getScreenHeight(getContext()) + topMargin)).setInterpolator(new OvershootInterpolator()).start();
                 break;
             case DIRECTION_LEFT:
-                distance = leftMargin + mWidth;
-                animate().xBy(isIn ? distance : -distance).setInterpolator(new OvershootInterpolator()).start();
+                animate().x(isIn ? (leftMargin + mWidth) : -(rightMargin + mWidth)).setInterpolator(new OvershootInterpolator()).start();
                 break;
             case DIRECTION_RIGHT:
-                distance = rightMargin + mWidth;
-                animate().xBy(isIn ? -distance : distance).setInterpolator(new OvershootInterpolator()).start();
+                animate().x(isIn ? (DensityUtil.getScreenWidth(getContext()) - (mWidth + rightMargin)) : (DensityUtil.getScreenWidth(getContext()) + leftMargin)).setInterpolator(new OvershootInterpolator()).start();
                 break;
         }
         isIn = !isIn;
     }
+
+    /*
+    // 方案二
+    public void slide(){
+        switch (direction){
+            case DIRECTION_TOP:
+                animate().y(isIn ? (topMargin + mHeight) : -(bottomMargin + mHeight)).setInterpolator(new OvershootInterpolator()).start();
+                break;
+            case DIRECTION_BOTTOM:
+                animate().y(isIn ? (DensityUtil.getScreenHeight(getContext()) - (mHeight + bottomMargin)) : (DensityUtil.getScreenHeight(getContext()) + topMargin)).setInterpolator(new OvershootInterpolator()).start();
+                break;
+            case DIRECTION_LEFT:
+                animate().x(isIn ? (leftMargin + mWidth) : -(rightMargin + mWidth)).setInterpolator(new OvershootInterpolator()).start();
+                break;
+            case DIRECTION_RIGHT:
+                animate().x(isIn ? (DensityUtil.getScreenWidth(getContext()) - (mWidth + rightMargin)) : (DensityUtil.getScreenWidth(getContext()) + leftMargin)).setInterpolator(new OvershootInterpolator()).start();
+                break;
+        }
+        isIn = !isIn;
+    }*/
+
+    /*
+    // 方案三
+    public void slide(){
+        switch (direction){
+            case DIRECTION_TOP:
+                if (isIn){
+                    setVisibility(VISIBLE);
+                    animate().y(topMargin).setInterpolator(new OvershootInterpolator()).start();
+                }else {
+                    setVisibility(GONE);
+                    setY(-(bottomMargin + mHeight));
+                }
+                break;
+            case DIRECTION_BOTTOM:
+                if (isIn){
+                    setVisibility(VISIBLE);
+                    animate().y((heightLimit - (mHeight + bottomMargin))).setInterpolator(new OvershootInterpolator()).start();
+                }else {
+                    setVisibility(GONE);
+                    setY(heightLimit + topMargin);
+                }
+                break;
+            case DIRECTION_LEFT:
+                if (isIn){
+                    setVisibility(VISIBLE);
+                    animate().x(leftMargin).setInterpolator(new OvershootInterpolator()).start();
+                }else {
+                    setVisibility(GONE);
+                    setX(-(rightMargin + mWidth));
+                }
+                break;
+            case DIRECTION_RIGHT:
+                if (isIn){
+                    setVisibility(VISIBLE);
+                    animate().x((widthLimit - (mWidth + rightMargin))).setInterpolator(new OvershootInterpolator()).start();
+                }else {
+                    setVisibility(GONE);
+                    setX(widthLimit + leftMargin);
+                }
+                break;
+        }
+        isIn = !isIn;
+    }*/
 }
